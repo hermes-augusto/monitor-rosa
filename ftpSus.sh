a19cad969a530cd87b4a3912bd4a6792c430ecba
hermes-augusto <hermes.barboza@gmail.com>
2022-03-17 20:59:52 -0300

    Ajuste na parte e conversão para csv

--- ftpSus.sh
+++
@@ -1,3 +0,0 @@
-DIR_ORIGEM=$1
-ARQ_ORIGEM=$2
-wget -P ~/monitor_rosa --user=Anonymous password= ftp://ftp.datasus.gov.br/${DIR_ORIGEM}/${ARQ_ORIGEM}
--- scripts/python/dbf2cvs.py
+++ scripts/python/dbf2cvs.py
@@ -3,22 +3,25 @@ import sys
 from datetime import datetime
 
 from dbfread import DBF
-def dbf_to_csv(dbf_table_pth): #Input a dbf, output a csv, same name, same path, except extension
+
+def dbf_to_csv(dbf_table_pth): 
     
     file_name=dbf_table_pth
-    dbf_table_pth = "/home/falkor/monitor_rosa/files/dbf_copy/ABOCE1502.dbf"
-    csv_fn = "/home/falkor/monitor_rosa/files/csv/"+file_name[:-4]+".csv" #Set the csv file name
-    table = DBF(dbf_table_pth, encoding="Latin-1")# table variable is a DBF object
-    with open(csv_fn, 'w', newline = '',encoding="utf8") as f:# create a csv file, fill it with dbf content
+    dbf_table_pth = "/home/falkor/monitor_rosa/files/dbf/"+file_name
+    csv_fn = "/home/falkor/monitor_rosa/files/csv/"+file_name[:-4]+".csv" 
+    table = DBF(dbf_table_pth, encoding="Latin-1")
+    #print("Inciando processamento: ",datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open(csv_fn, 'w', newline = '',encoding="utf8") as f:
         writer = csv.writer(f)
-        writer.writerow(table.field_names)# write the column name
-        for record in table: # write the rows
+        writer.writerow(table.field_names)
+        for record in table: 
             writer.writerow(list(record.values()))
-    return csv_fn # return the csv name
+    #print("Fim do processamento: ",datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    return csv_fn
 
 if __name__ == "__main__":
-    #print("Inciando processamento: ",datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    print("Inciando processamento: ",datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     name_file = sys.argv[1]
     dbf_to_csv(name_file)
-    #print("Fim do processamento: ",datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    print("Fim do processamento: ",datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
